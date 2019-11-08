ac23bfd1ced63c9712575dc07823b41be9153326
Haley Leonard <Haley.M.Leonard-1@ou.edu>
2019-11-08 09:52:47 -0600

    creating coffee quiz

--- HalysseGroupProject/BuzzfeedQuiz/coffeeQuiz.xaml.cs
+++ HalysseGroupProject/BuzzfeedQuiz/coffeeQuiz.xaml.cs
@@ -23,5 +23,77 @@ namespace BuzzfeedQuiz
         {
             InitializeComponent();
         }
+
+      
+
+        private void CofresultsBTN_Click_1(object sender, RoutedEventArgs e)
+        {
+            int ques1AnswerID = 0, ques2AnswerID = 0, ques3AnswerID = 0, ques4AnswerID = 0, answerTotal = 0;
+            pslResults psl = new pslResults();
+            americanoResults ar = new americanoResults();
+            vanillaResults vr = new vanillaResults();
+
+            if (mornRB.IsChecked == true)
+            {
+                ques1AnswerID = 1;
+            }
+            else if (nightRB.IsChecked == true)
+            {
+                ques1AnswerID = 2;
+            }
+            if (purpRB.IsChecked == true)
+            {
+                ques2AnswerID = 1;
+            }
+            else if (orangeRB.IsChecked == true)
+            {
+                ques2AnswerID = 2;
+            }
+            else if (blueRB.IsChecked == true)
+            {
+                ques2AnswerID = 3;
+            }
+            else if (BlackRB.IsChecked == true)
+            {
+                ques2AnswerID = 4;
+            }
+            if (catRB.IsChecked == true)
+            {
+                ques3AnswerID = 1;
+            }
+            else if (dogsRB.IsChecked == true)
+            {
+                ques3AnswerID = 2;
+            }
+            if (moviesRB.IsChecked == true)
+            {
+                ques4AnswerID = 1;
+            }
+            else if (shopRB.IsChecked == true)
+            {
+                ques4AnswerID = 2;
+            }
+            else if (hikeRB.IsChecked == true)
+            {
+                ques4AnswerID = 3;
+            }
+            else if (sleepRB.IsChecked == true)
+            {
+                ques4AnswerID = 4;
+            }
+            answerTotal = ques1AnswerID + ques2AnswerID + ques3AnswerID + ques4AnswerID;
+            if (answerTotal <= 5)
+            {
+                vr.Show();
+            }
+            else if (answerTotal <= 9)
+            {
+                psl.Show();
+            }
+            else if (answerTotal >= 10)
+            {
+                ar.Show();
+            }
+        }
     }
 }
---
+++ HalysseGroupProject/BuzzfeedQuiz/pslResults.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BuzzfeedQuiz
+{
+    /// <summary>
+    /// Interaction logic for pslResults.xaml
+    /// </summary>
+    public partial class pslResults : Window
+    {
+        public pslResults()
+        {
+            InitializeComponent();
+
+            //string imgUrl = "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj6urbV-drlAhVFPK0KHdtZCKMQjRx6BAgBEAQ&url=https%3A%2F%2Ffofarms.com%2Fpost%2Fnot-your-basic-coffee-diy-pumpkin-spice-latte-recipe%2F&psig=AOvVaw3KqH02408v4zhJObTgsTod&ust=1573314026414817";
+            //BitmapImage bitmap = new BitmapImage();
+            //bitmap.BeginInit();
+            //bitmap.UriSource = new Uri(imgUrl);
+            //bitmap.EndInit();
+
+            //pslImg.Source = bitmap;
+        }
+    }
+}
