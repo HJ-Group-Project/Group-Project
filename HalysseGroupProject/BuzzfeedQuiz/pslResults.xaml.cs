5a60cbf05fc0d04196d62cb7fcd9854451de93e3
Haley Leonard <Haley.M.Leonard-1@ou.edu>
2019-11-11 15:15:12 -0600

    editing coffeequiz and starting spongebob quiz

--- HalysseGroupProject/BuzzfeedQuiz/pslResults.xaml.cs
+++ HalysseGroupProject/BuzzfeedQuiz/pslResults.xaml.cs
@@ -23,13 +23,12 @@ namespace BuzzfeedQuiz
         {
             InitializeComponent();
 
-            //string imgUrl = "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj6urbV-drlAhVFPK0KHdtZCKMQjRx6BAgBEAQ&url=https%3A%2F%2Ffofarms.com%2Fpost%2Fnot-your-basic-coffee-diy-pumpkin-spice-latte-recipe%2F&psig=AOvVaw3KqH02408v4zhJObTgsTod&ust=1573314026414817";
-            //BitmapImage bitmap = new BitmapImage();
-            //bitmap.BeginInit();
-            //bitmap.UriSource = new Uri(imgUrl);
-            //bitmap.EndInit();
-
-            //pslImg.Source = bitmap;
+            string imgUrl = "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj6urbV-drlAhVFPK0KHdtZCKMQjRx6BAgBEAQ&url=https%3A%2F%2Ffofarms.com%2Fpost%2Fnot-your-basic-coffee-diy-pumpkin-spice-latte-recipe%2F&psig=AOvVaw3KqH02408v4zhJObTgsTod&ust=1573314026414817";
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(imgUrl);
+            bitmap.EndInit();
+            pslImg.Source = bitmap;
         }
     }
 }
