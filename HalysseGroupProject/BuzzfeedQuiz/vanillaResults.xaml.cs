de05ea2d6a7391a75a912c83cf2e0fdd301078d9
Haley Leonard <Haley.M.Leonard-1@ou.edu>
2019-11-16 11:59:11 -0600

    inserting images

--- HalysseGroupProject/BuzzfeedQuiz/vanillaResults.xaml.cs
+++ HalysseGroupProject/BuzzfeedQuiz/vanillaResults.xaml.cs
@@ -23,12 +23,6 @@ namespace BuzzfeedQuiz
         {
             InitializeComponent();
 
-            string imgUrl = "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiK0u2k_uLlAhUEba0KHZibCdwQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.tasteslovely.com%2Fwhole30-vanilla-latte-coffee%2F&psig=AOvVaw24VIio6WW8cT5vG9-g_E9s&ust=1573590140954749";
-            BitmapImage bitmap = new BitmapImage();
-            bitmap.BeginInit();
-            bitmap.UriSource = new Uri(imgUrl);
-            bitmap.EndInit();
-            vanResImg.Source = bitmap;
         }
     }
 }
