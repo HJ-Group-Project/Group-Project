cad93f9dcdd5fb6e2f0703bd90ec40d07e52984c
Haley Leonard <Haley.M.Leonard-1@ou.edu>
2019-11-18 11:16:01 -0600

    storing answers in db

--- HalysseGroupProject/BuzzfeedQuiz/spongebobQuiz.xaml.cs
+++ HalysseGroupProject/BuzzfeedQuiz/spongebobQuiz.xaml.cs
@@ -22,7 +22,11 @@ namespace BuzzfeedQuiz
         public spongebobQuiz()
         {
             InitializeComponent();
+            
+
         }
+
+        DB_128040_quizletEntities db = new DB_128040_quizletEntities();
 
         private void ResultsBTN_Click(object sender, RoutedEventArgs e)
         {
@@ -33,69 +37,87 @@ namespace BuzzfeedQuiz
             KrabsResults kr = new KrabsResults();
             SquidwardResults sqr = new SquidwardResults();
 
+            TAnswer answer = new TAnswer();
+
             if (musicRB.IsChecked == true)
             {
                 ques1AnswerID = 1;
+                answer.AnswerOne = "1";
             }
             else if (movieRB.IsChecked == true)
             {
                 ques1AnswerID = 2;
+                answer.AnswerOne = "2";
             }
             else if (hangRB.IsChecked == true)
             {
                 ques1AnswerID = 3;
+                answer.AnswerOne = "3";
             }
             else if (workRB.IsChecked == true)
             {
                 ques1AnswerID = 4;
+                answer.AnswerOne = "4";
             }
             if (greenRB.IsChecked == true)
             {
                 ques2AnswerID = 1;
+                answer.AnswerTwo = "1";
             }
             else if (pinkRB.IsChecked == true)
             {
                 ques2AnswerID = 2;
+                answer.AnswerTwo = "2";
             }
             else if (purpRB.IsChecked == true)
             {
                 ques2AnswerID = 3;
+                answer.AnswerTwo = "3";
             }
             else if (orangeRB.IsChecked == true)
             {
                 ques2AnswerID = 4;
+                answer.AnswerTwo = "3";
             }
             if(crabRB.IsChecked==true)
             {
                 ques3AnswerID = 1;
+                answer.AnswerThree = "1";
             }
             else if(pizzaRB.IsChecked==true)
             {
                 ques3AnswerID = 2;
+                answer.AnswerThree = "2";
             }
             else if(spagRB.IsChecked==true)
             {
                 ques3AnswerID = 3;
+                answer.AnswerThree = "3";
             }
             else if(friesRB.IsChecked==true)
             {
                 ques3AnswerID = 4;
+                answer.AnswerThree = "4";
             }
             if (garyRB.IsChecked == true)
             {
                 ques4AnswerID = 1;
+                answer.AnswerFour = "1";
             }
             else if (patRB.IsChecked == true)
             {
                 ques4AnswerID = 2;
+                answer.AnswerFour = "2";
             }
             else if (plankRB.IsChecked == true)
             {
                 ques4AnswerID = 3;
+                answer.AnswerFour = "3";
             }
             else if (sandRB.IsChecked == true)
             {
                 ques4AnswerID = 4;
+                answer.AnswerFour = "4";
             }
             answerTotal = ques1AnswerID + ques2AnswerID + ques3AnswerID + ques4AnswerID;
             if (answerTotal <= 4)
@@ -114,6 +136,9 @@ namespace BuzzfeedQuiz
             {
                 kr.Show();
             }
+            answer.TQuiz = db.TQuizs.Find(14);
+            db.TAnswers.Add(answer);
+            db.SaveChanges();
         }
     }
 }
