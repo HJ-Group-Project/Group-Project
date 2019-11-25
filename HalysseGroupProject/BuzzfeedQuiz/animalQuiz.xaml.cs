bd744964d0fde96181f8c39b3d9ae96d1914efc4
Jalysse Brown <jalysse.brown@ou.edu>
2019-11-25 11:49:08 -0600

    36

--- HalysseGroupProject/BuzzfeedQuiz/animalQuiz.xaml.cs
+++ HalysseGroupProject/BuzzfeedQuiz/animalQuiz.xaml.cs
@@ -28,17 +28,114 @@ namespace BuzzfeedQuiz
 
         private void ResultusBttn_Click(object sender, RoutedEventArgs e)
         {
-            int Aques1AnswerID = 0, Aques2AnswerID = 0, Aques3AnswerID = 0, Aques4AnswerID = 0, AanswerTotal = 0;
+            int ques1AnswerID = 0, ques2AnswerID = 0, ques3AnswerID = 0, ques4AnswerID = 0, answerTotal = 0;
             AlligatorResults ar = new AlligatorResults();
             HorseResult hr = new HorseResult();
             LionResults lr = new LionResults();
             CoyoteResults cr = new CoyoteResults();
-        }
-        TAnswer answer = new TAnswer();
 
-        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
-        {
+            TAnswer answer = new TAnswer();
 
+            if (comfortRB.IsChecked == true)
+            {
+                ques1AnswerID = 1;
+                answer.AnswerOne = "1";
+            }
+            else if (comegoRB.IsChecked == true)
+            {
+                ques1AnswerID = 2;
+                answer.AnswerOne = "2";
+            }
+            else if (spendRB.IsChecked == true)
+            {
+                ques1AnswerID = 3;
+                answer.AnswerOne = "2";
+            }
+            else if (friendsRB.IsChecked == true)
+            {
+                ques1AnswerID = 4;
+                answer.AnswerOne = "2";
+            }
+            if (strikeRB.IsChecked == true)
+            {
+                ques1AnswerID = 1;
+                answer.AnswerOne = "1";
+            }
+            else if (walkRB.IsChecked == true)
+            {
+                ques1AnswerID = 2;
+                answer.AnswerOne = "2";
+            }
+            else if (rageRB.IsChecked == true)
+            {
+                ques1AnswerID = 3;
+                answer.AnswerOne = "3";
+            }
+            else if (callRB.IsChecked == true)
+            {
+                ques1AnswerID = 4;
+                answer.AnswerOne = "4";
+            }
+            if (myselfRB.IsChecked == true)
+            {
+                ques1AnswerID = 1;
+                answer.AnswerOne = "1";
+            }
+            else if (strangerRB.IsChecked == true)
+            {
+                ques1AnswerID = 2;
+                answer.AnswerOne = "2";
+            }
+            else if (familyRB.IsChecked == true)
+            {
+                ques1AnswerID = 3;
+                answer.AnswerOne = "3";
+            }
+            else if (loyalRB.IsChecked == true)
+            {
+                ques1AnswerID = 4;
+                answer.AnswerOne = "4";
+            }
+            if (secretRB.IsChecked == true)
+            {
+                ques1AnswerID = 1;
+                answer.AnswerOne = "1";
+            }
+            else if (bookRB.IsChecked == true)
+            {
+                ques1AnswerID = 2;
+                answer.AnswerOne = "2";
+            }
+            else if (lifeRB.IsChecked == true)
+            {
+                ques1AnswerID = 3;
+                answer.AnswerOne = "3";
+            }
+            else if (truthRB.IsChecked == true)
+            {
+                ques1AnswerID = 4;
+                answer.AnswerOne = "4";
+            }
+            answerTotal = ques1AnswerID + ques2AnswerID + ques3AnswerID + ques4AnswerID;
+            if (answerTotal <= 4)
+            {
+                ar.Show();
+            }
+            else if (answerTotal <= 6)
+            {
+                hr.Show();
+            }
+            else if (answerTotal <= 8)
+            {
+                lr.Show();
+            }
+            else if (answerTotal >= 10)
+            {
+                cr.Show();
+            }
+            answer.TQuiz = db.TQuizs.Find(14);
+            db.TAnswers.Add(answer);
+            db.SaveChanges();
         }
     }
 }
